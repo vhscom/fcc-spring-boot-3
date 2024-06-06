5e04276d4b1048a1b785110cb1c8715f5fad5ad7
Josh Habdas <habdasj@objectcomputing.com>
2024-06-06 17:53:47 -0400

    right way repo with dep injection

--- src/main/java/com/vhsdev/runnerz/run/RunController.java
+++ src/main/java/com/vhsdev/runnerz/run/RunController.java
@@ -1,24 +1,22 @@
 package com.vhsdev.runnerz.run;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RunController {
 
-  /**
-   * With Spring being IoC, this would be the wrong way to go about accessing the repository.
-   * This is an issue because me and 800 friends could hit this controller and we'll get a new
-   * instance of the RunRepository each time. Instead we want to treat RunRepository as a singleton.
-   *
-   * @see https://www.youtube.com/watch?v=31KTdfRH6nY (1:01:11) for dependency injection info.
-   */
   private final RunRepository repository;
+  private final RunRepository runRepository;
 
-  public RunController() {
-    this.repository = new RunRepository();
+  public RunController(RunRepository runRepository) {
+    this.repository = runRepository;
+    this.runRepository = runRepository;
   }
 
+  @GetMapping("/api/runs")
   List<Run> findAll() {
-    return null;
+    return runRepository.findAll();
   }
 }
