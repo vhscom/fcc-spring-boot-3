56898da60d88bd3545d804af7ecb47f0d99cea94
Josh Habdas <habdasj@objectcomputing.com>
2024-06-07 17:37:38 -0400

    begin reimplementing repo with jdbc

--- src/main/java/com/vhsdev/runnerz/run/RunRepository.java
+++ src/main/java/com/vhsdev/runnerz/run/RunRepository.java
@@ -1,46 +1,24 @@
 package com.vhsdev.runnerz.run;
 
-import jakarta.annotation.PostConstruct;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RunRepository {
 
-  private List<Run> runs = new ArrayList<>();
+  private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+  private final JdbcClient jdbcClient;
 
-  List<Run> findAll() {
-    return runs;
+  public RunRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
   }
 
-  Optional<Run> findById(Integer id) {
-    return runs.stream()
-        .filter(run -> run.id().equals(id))
-        .findFirst();
-  }
-
-  void create(Run run) {
-    runs.add(run);
-  }
-
-  public void update(Run run, Integer id) {
-    Optional<Run> existingRun = findById(id);
-    existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
-  }
-
-  public void delete(Integer id) {
-    findById(id).ifPresent(runs::remove);
-  }
-
-  @PostConstruct
-  private void init() {
-    runs.add(new Run(1, "Morning Run", LocalDateTime.now(),
-        LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 5, Location.INDOOR));
-    runs.add(new Run(2, "Evening Run", LocalDateTime.now(),
-        LocalDateTime.now().plus(1, ChronoUnit.HOURS), 10, Location.OUTDOOR));
+  public List<Run> findAll() {
+    return jdbcClient.sql("select * from run")
+        .query(Run.class)
+        .list();
   }
 }
