b7e4c4b9b652ffcb9a7853f32ac6f537b166320b
Josh Habdas <habdasj@objectcomputing.com>
2024-06-07 15:58:35 -0400

    add schema.sql file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+/**
+ * By default, Spring Boot will use an embedded database (H2) when you use the spring-boot-starter-data-jpa "Starter".
+ * By convention, it will look for data.sql and schema.sql files in the root of the classpath.
+ */
