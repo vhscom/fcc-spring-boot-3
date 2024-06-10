61d732bdd698559822fbfcf71e913f5c382a3a7a
Josh Habdas <habdasj@objectcomputing.com>
2024-06-10 17:15:51 -0400

    write passing unit test for repo

--- src/test/java/com/vhsdev/runnerz/run/InMemoryRunRepositoryTest.java
+++ src/test/java/com/vhsdev/runnerz/run/InMemoryRunRepositoryTest.java
@@ -2,6 +2,31 @@ package com.vhsdev.runnerz.run;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class InMemoryRunRepositoryTest {
 
+  InMemoryRunRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    repository = new InMemoryRunRepository();
+    repository.create(
+        new Run(1, "Monday Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(30,
+            ChronoUnit.MINUTES), 3,
+            Location.INDOOR, null));
+    repository.create(new Run(2, "Wednesday Evening Run", LocalDateTime.now(),
+        LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6,
+        Location.INDOOR, null));
+  }
+
+  @Test
+  void shouldFindAllRuns() {
+    List<Run> runs = repository.findAll();
+    assertEquals(2, runs.size(), "Should find 2 runs");
+  }
 }
