5098469227da69ab6866fd374973ae6d62aaedbb
Josh Habdas <habdasj@objectcomputing.com>
2024-06-10 10:54:20 -0400

    stub out additional repo methods

--- src/main/java/com/vhsdev/runnerz/run/RunRepository.java
+++ src/main/java/com/vhsdev/runnerz/run/RunRepository.java
@@ -25,25 +25,54 @@ public class RunRepository {
   }
 
   public Optional<Run> findById(Integer id) {
-    return jdbcClient.sql("select id,title,started_on,completed_on,miles,location from run where id = :id")
+    return jdbcClient.sql(
+            "select id,title,started_on,completed_on,miles,location from run where id = :id")
         .param("id", id)
         .query(Run.class)
         .optional();
   }
 
   public void create(Run run) {
-    var updated = jdbcClient.sql("insert into run (id,title,started_on,completed_on,miles,location) values (?,?,?,?,?,?)")
-        .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString()))
+    var updated = jdbcClient.sql(
+            "insert into run (id,title,started_on,completed_on,miles,location) values (?,?,?,?,?,?)")
+        .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(),
+            run.location().toString()))
         .update();
 
     Assert.state(updated == 1, "Failed to create run" + run.title());
   }
 
   public void update(Run run, Integer id) {
-    var updated = jdbcClient.sql("update run set title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? where id = ?")
-        .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString(), id))
+    var updated = jdbcClient.sql(
+            "update run set title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? where id = ?")
+        .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.miles(),
+            run.location().toString(), id))
         .update();
 
     Assert.state(updated == 1, "Failed to update run" + run.title());
   }
+
+  public void delete(Integer id) {
+    var updated = jdbcClient.sql("delete from run where id = ?")
+        .param("id", id)
+        .update();
+
+    Assert.state(updated == 1, "Failed to delete run" + id);
+  }
+
+  public int count() {
+    return jdbcClient.sql("select * from run").query().listOfRows().size();
+  }
+
+  public void saveAll(List<Run> runs) {
+    runs.stream().forEach(this::create);
+  }
+
+  public List<Run> findByLocation(Location location) {
+    return jdbcClient.sql("select * from run where location = :location")
+        .param("location", location) // not using toString() here
+        .query(Run.class)
+        .list();
+  }
+
 }
