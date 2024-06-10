f02630d25de14f103612eab9fe91597fb5e841d3
Josh Habdas <habdasj@objectcomputing.com>
2024-06-10 11:06:59 -0400

    add http endpoint collection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into run (id, title, started_on, completed_on, miles, location)
 values (1, 'Morning Run', '2020-01-01 06:00:00', '2020-01-01 07:00:00', 5, 'OUTDOOR');
+values (2, 'Evening Run', '2020-01-01 18:00:00', '2020-01-01 19:00:00', 3, 'INDOOR');
