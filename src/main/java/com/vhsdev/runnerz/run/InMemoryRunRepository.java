99ab044a62c32eb8fc04cf800a032fe991be6e26
Josh Habdas <habdasj@objectcomputing.com>
2024-06-10 16:54:02 -0400

    restore in memory repo for testing

---
+++ src/main/java/com/vhsdev/runnerz/run/InMemoryRunRepository.java
@@ -0,0 +1,46 @@
+package com.vhsdev.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InMemoryRunRepository {
+
+  private List<Run> runs = new ArrayList<>();
+
+  List<Run> findAll() {
+    return runs;
+  }
+
+  Optional<Run> findById(Integer id) {
+    return runs.stream()
+        .filter(run -> run.id().equals(id))
+        .findFirst();
+  }
+
+  void create(Run run) {
+    runs.add(run);
+  }
+
+  public void update(Run run, Integer id) {
+    Optional<Run> existingRun = findById(id);
+    existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+  }
+
+  public void delete(Integer id) {
+    findById(id).ifPresent(runs::remove);
+  }
+
+  @PostConstruct
+  private void init() {
+    runs.add(new Run(1, "Morning Run", LocalDateTime.now(),
+        LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 5, Location.INDOOR, 0));
+    runs.add(new Run(2, "Evening Run", LocalDateTime.now(),
+        LocalDateTime.now().plus(1, ChronoUnit.HOURS), 10, Location.OUTDOOR, 0));
+  }
+}
