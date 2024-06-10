9da5968d9f6c28449985a9d516b5c84070339597
Josh Habdas <habdasj@objectcomputing.com>
2024-06-10 17:00:25 -0400

    update in memory class from example

--- src/main/java/com/vhsdev/runnerz/run/InMemoryRunRepository.java
+++ src/main/java/com/vhsdev/runnerz/run/InMemoryRunRepository.java
@@ -6,10 +6,14 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryRunRepository {
+
+  private static final Logger log = LoggerFactory.getLogger(InMemoryRunRepository.class);
 
   private List<Run> runs = new ArrayList<>();
 
@@ -18,13 +22,16 @@ public class InMemoryRunRepository {
   }
 
   Optional<Run> findById(Integer id) {
-    return runs.stream()
+    return Optional.ofNullable(runs.stream()
         .filter(run -> run.id().equals(id))
-        .findFirst();
+        .findFirst()
+        .orElseThrow(RunNotFoundException::new));
   }
 
   void create(Run run) {
-    runs.add(run);
+    Run newRun = new Run(run.id(), run.title(), run.startedOn(), run.completedOn(),
+        run.miles(), run.location(), null);
+    runs.add(newRun);
   }
 
   public void update(Run run, Integer id) {
