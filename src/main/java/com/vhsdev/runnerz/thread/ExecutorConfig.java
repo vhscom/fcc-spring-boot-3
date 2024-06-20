ed63d5596e2c65d03ec8df98f6d13e4a4db15378
Josh Habdas <habdasj@objectcomputing.com>
2024-06-20 13:11:01 -0400

    dynamically allocate virtual threads

--- src/main/java/com/vhsdev/runnerz/thread/ExecutorConfig.java
+++ src/main/java/com/vhsdev/runnerz/thread/ExecutorConfig.java
@@ -2,17 +2,27 @@ package com.vhsdev.runnerz.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExecutorConfig {
 
+  private static final Logger log = LoggerFactory.getLogger(ExecutorConfig.class);
+
+  // This method returns the number of cores available in the system
+  static int numberOfCores() {
+    log.info("Number of cores: {}", Runtime.getRuntime().availableProcessors());
+    return Runtime.getRuntime().availableProcessors();
+  }
+
   // Uncomment this method to use the ThrottledVirtualThreadsExecutor class
   // This method creates a ThrottledVirtualThreadsExecutor bean with N virtual threads
   @Bean(name = "virtualThreadExecutor")
   public ThrottledVirtualThreadsExecutor virtualThreadExecutor() {
-    return new ThrottledVirtualThreadsExecutor(10); // 10 virtual threads
+    return new ThrottledVirtualThreadsExecutor(numberOfCores());
   }
 
   // Uncomment this method to use the newVirtualThreadPerTaskExecutor method
